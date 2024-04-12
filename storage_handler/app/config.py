64e32f32cad42a468abf00913bdf9b8ca836b8bd
Leonid Pantaler <90leonid@gmail.com>
2024-04-12 15:32:54 +0300

    update storage handler

--- storage_handler/app/config.py
+++ storage_handler/app/config.py
@@ -1,30 +1,24 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 folder = "storage_handler"
 
 
 class AppSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=f"{folder}/.env.app", env_file_encoding='utf-8')
+
     storage_handler_port: str
     app_folder: str
 
-    class Config:
-        env_file = f"{folder}/.env.app"
-
-
 class PublicSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=f"{folder}/.env", env_file_encoding='utf-8')
     app_data_folder: str
-
-    class Config:
-        env_file = f"{folder}/.env"
-
 
 app_settings = AppSettings()
 public_settings = PublicSettings()
 
 
 class Settings(BaseSettings):
-    class Config:
-        extra = "allow"
+    model_config = SettingsConfigDict(extra = "allow")
 
 
 settings = Settings(**{**AppSettings().model_dump(), **PublicSettings().model_dump()})
--- storage_handler/tests/config.py
+++ storage_handler/tests/config.py
@@ -1,16 +1,14 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from storage_handler.app.config import folder, AppSettings, PublicSettings
 
-PublicSettings.Config.env_file = f"{folder}/.env.test"
+PublicSettings.model_config.update(env_file=f"{folder}/.env.test")
 
 app_settings = AppSettings()
 public_settings = PublicSettings()
 
 
 class Settings(BaseSettings):
-    class Config:
-        extra = "allow"
-
+    model_config = SettingsConfigDict(extra="allow")
 
 settings = Settings(**{**AppSettings().model_dump(), **PublicSettings().model_dump()})
